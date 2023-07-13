c6ed8d293052053736c4117fa679019368746c8e
Sarawut Pe <mr.sarawutpe@gmail.com>
2023-07-13 23:45:32 +0700

    .

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  useEffect(() => {
+    axios
+      .get("https://express-q3ki.onrender.com", { withCredentials: true })
+      .then((v) => {
+        console.log(v);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
